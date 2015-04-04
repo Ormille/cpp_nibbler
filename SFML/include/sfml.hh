991894bec4828b5e79c9ccd19cd94c3d419def78
Ormille <julie.terranova@epitech.eu>
2015-04-04 21:59:30 +0200

    fini sauf pour affTxt

--- SFML/include/sfml.hh
+++ SFML/include/sfml.hh
@@ -5,18 +5,23 @@
 // Login   <terran_j@epitech.net>
 //
 // Started on  Tue Mar 24 17:01:57 2015 Julie Terranova
-// Last update Sat Apr  4 18:53:31 2015 terran_j
+// Last update Sat Apr  4 21:54:05 2015 terran_j
 //
 
 #ifndef SFML_HH__
 # define SFML_HH__
 
+#include <map>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include "IObjGraph.hpp"
 
 #define SIZE 35
+
+class SFML;
+
+typedef void (SFML::*mapFPtr)(int, int);
 
 class SFML : public IObjGraph
 {
@@ -25,16 +30,38 @@ public:
   ~SFML();
 
 private:
+  std::map<int, mapFPtr> _mapf;
+  std::map<int, int> _mape;
+
+  SFML &operator=(SFML const &);
+  SFML(const SFML &);
+  SFML(const SFML &&);
+
   int initLib(unsigned int x, unsigned int y);
   int getEvent();
   void refreshImg(int **map);
   void affText(const std::string &toAff);
   void closeLib();
 
+  void buildMaps();
+  void fbords(int x, int y);
+  void ftete(int x, int y);
+  void fsnake(int x, int y);
+  void fqueue(int x, int y);
+  void fswimm(int x, int y);
+  void fportal(int x, int y);
+  void ffruit1(int x, int y);
+  void ffruit2(int x, int y);
+  void ffruit3(int x, int y);
+  void ffruit4(int x, int y);
+  void ffruit5(int x, int y);
+  void ffruit6(int x, int y);
+  void ffruit7(int x, int y);
+  void ffruit8(int x, int y);
+
   sf::RenderWindow *_window;
   int _x;
   int _y;
-  sf::Sprite _background;
   sf::Sprite _bords;
   sf::Sprite _snake;
   sf::Sprite _tete;
